74bbc780091ca478cb7aba05894d65aa8f6eb035
destys <desstyss@gmail.com>
2025-09-11 13:27:10 +0300

    header adaptive

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
     <header className="fixed top-0 left-0 z-50 w-full flex items-center justify-between gap-3.5 xl:gap-7 2xl:gap-14 bg-white h-14 md:h-[70px]">
       <Link
         href={"/"}
-        className="flex justify-center items-center basis-[140px] xl:basis-[170px] h-[70px] bg-aqua-base shrink-0"
+        className="flex justify-center items-center basis-[140px] xl:basis-[170px] h-14 md:h-[70px] bg-aqua-base shrink-0"
       >
         <Image
           src={"/logo_horisontal.svg"}
